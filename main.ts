dd01e876c38c0e8bb4ad1227425f7e5784480085
J0EK3R <J0EK3R@users.noreply.github.com>
2025-01-29 12:45:17 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,27 @@
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-    MK.stop(MK6.Module.M1)
-    MK.stop(MK6.Module.M2)
+    MK6.stop(MK6.Module.M1)
+    MK6.stop(MK6.Module.M2)
 })
 input.onButtonPressed(Button.A, function () {
     if (hub == 0) {
         speed1 += 10
-        MK.setChannel(MK6.Module.M1, MK6.Channel.A, speed1)
-        MK.setData(MK6.Module.M1)
+        MK6.setChannel(MK6.Module.M1, MK6.Channel.A, speed1)
+        MK6.setData(MK6.Module.M1)
     } else if (hub == 1) {
         speed2 += 10
-        MK.setChannel(MK6.Module.M2, MK6.Channel.A, speed2)
-        MK.setData(MK6.Module.M2)
+        MK6.setChannel(MK6.Module.M2, MK6.Channel.A, speed2)
+        MK6.setData(MK6.Module.M2)
     }
 })
 input.onButtonPressed(Button.B, function () {
     if (hub == 0) {
         speed1 += -10
-        MK.setChannel(MK6.Module.M1, MK6.Channel.A, speed1)
-        MK.setData(MK6.Module.M1)
+        MK6.setChannel(MK6.Module.M1, MK6.Channel.A, speed1)
+        MK6.setData(MK6.Module.M1)
     } else if (hub == 1) {
         speed2 += -10
-        MK.setChannel(MK6.Module.M2, MK6.Channel.A, speed2)
-        MK.setData(MK6.Module.M2)
+        MK6.setChannel(MK6.Module.M2, MK6.Channel.A, speed2)
+        MK6.setData(MK6.Module.M2)
     }
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
@@ -36,8 +36,8 @@ let hub = 0
 hub = 0
 speed1 = 0
 speed2 = 0
-MK.init(MK6.Module.M2)
-MK.init(MK6.Module.M1)
+MK6.init(MK6.Module.M1)
+MK6.init(MK6.Module.M2)
 basic.forever(function () {
     if (hub == 0) {
         basic.showNumber(speed1)
